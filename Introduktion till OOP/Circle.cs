bdcaa71b1b2ae26384e908838967099a7233f121
Bellanthie <bellanthie@gmail.com>
2025-10-26 20:37:14 +0100

    Jag är EJ färdig och lämnar in den som den är för att jobba vidare med den.

--- Introduktion till OOP/Circle.cs
+++ Introduktion till OOP/Circle.cs
@@ -8,35 +8,51 @@ namespace Introduktion_till_OOP
 {
     public class Circle
     {
-        // Constructor that welcomes the radius as a parameter
         private double radius;
 
-        // Parameterless constructor (default radius = 1.0)
         public Circle()
         {
-            radius = 1.0;
+            this.radius = 1.0;
         }
 
-        // Constructor that receives the radius as a parameter
-        public Circle (double radius)
+        public Circle(double radius)
         {
             this.radius = radius;
         }
 
-        // Public property to get/set radius
         public double Radius
         {
             get => radius;
             set => radius = value;
         }
 
-        // Helper methods
         public double Area() => Math.PI * radius * radius;
-        public double Circumference() => 2 * Math.PI * radius;
-        //public double GetArea()
-        //{
-        //    return Math.PI * radius * radius;
-        //}
 
+        public double Circumference() => 2 * Math.PI * radius;
+
+        // Remove this method if not needed, as Area() is sufficient.
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
+
+// Move Program class outside Circle class
+namespace Introduktion_till_OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create with parameterized constructor
+            Circle circle1 = new Circle(5);
+
+            // Create using parameterless constructor plus object initializer
+            Circle circle2 = new Circle(6);
+
+            Console.WriteLine($"Circle1: Radius={circle1.Radius}, Area={circle1.Area():F2}, Circumference={circle1.Circumference():F2}");
+            Console.WriteLine($"Circle2: Radius={circle2.Radius}, Area={circle2.Area():F2}, Circumference={circle2.Circumference():F2}");
+        }
     }
 }
--- Introduktion till OOP/Program.cs
+++ Introduktion till OOP/Program.cs
@@ -1,17 +1,40 @@
-﻿namespace Introduktion_till_OOP
+﻿using System;
+
+namespace Introduktion_till_OOP
 {
     // Creating a 'Circle' class
     public class Circle
     {
         // Creating a class constructor for the Circle class
-        public int Radius; 
 
+        public int Radius { get; private set; }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
 
         // Method..add paranthesese in order to pass in information for later on
-        public int GetArea(int Radius)
+        public double Area()
         {
-            int number = Radius;
-            return number;
+            return Math.PI * Radius * Radius;
+        }
+
+        public double Circumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            Console.WriteLine($"Circle1: Radius={circle1.Radius}, Area={circle1.Area():F2}, Circumference={circle1.Circumference():F2}");
+            Console.WriteLine($"Circle2: Radius={circle2.Radius}, Area={circle2.Area():F2}, Circumference={circle2.Circumference():F2}");
         }
     }
 }
